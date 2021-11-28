ba68c9df669793c5c07db3e4f573993a783893b4
이준호 <a01032208149@gmail.com>
2021-11-28 18:04:45 +0900

    - 카메라 완성

--- hw5-2/src/main/java/com/example/hw5_2/MainActivity.java
+++ hw5-2/src/main/java/com/example/hw5_2/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.hw5_2;
 
 import android.content.Intent;
 import android.graphics.Bitmap;
+import android.graphics.Matrix;
 import android.net.Uri;
 import android.os.Bundle;
 import android.os.Environment;
@@ -10,10 +11,9 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.Toast;
-
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.content.FileProvider;
-
+import android.media.ExifInterface;
 import java.io.File;
 import java.io.IOException;
 import java.text.SimpleDateFormat;
@@ -61,10 +61,50 @@ public class MainActivity extends AppCompatActivity {
                 } catch (Exception e) {
                     e.printStackTrace();
                 }
+                int orientation = getOrientationOfImage(mCurrentPhotoPath);
+                try {
+                    bitmap = getRotatedBitmap(bitmap, orientation);
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
                 imageView.setImageBitmap(bitmap);
             } else if (resultCode == RESULT_CANCELED){
                 Toast.makeText(MainActivity.this, "Cancelled", Toast.LENGTH_SHORT).show();
             }
         }
+    }
+
+    public int getOrientationOfImage(String filepath)  {
+        ExifInterface exif = null;
+        try {
+            exif = new ExifInterface(filepath);
+        } catch (IOException e) {
+            e.printStackTrace();
+            return -1;
+        }
+
+        int orientation = exif.getAttributeInt(ExifInterface.TAG_ORIENTATION, ExifInterface.ORIENTATION_UNDEFINED);
+
+        if (orientation != 0){
+            switch(orientation) {
+                case ExifInterface.ORIENTATION_ROTATE_90:
+                    return 90;
+                case ExifInterface.ORIENTATION_ROTATE_180:
+                    return 180;
+                case ExifInterface.ORIENTATION_ROTATE_270:
+                    return 270;
+
+            }
+        }
+        return 0;
+    }
+
+    public Bitmap getRotatedBitmap(Bitmap bitmap, int degree) throws Exception{
+        if (bitmap == null) return null;
+        if (degree == 0) return bitmap;
+
+        Matrix m = new Matrix();
+        m.setRotate(degree, (float) bitmap.getWidth() / 2 , (float) bitmap.getHeight() / 2);
+        return Bitmap.createBitmap(bitmap, 0, 0, bitmap.getWidth(), bitmap.getHeight(), m, true); // Filter = True
     }
 }
